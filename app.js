c50c47daa83ca594e01408fc36647e7a33a34f9b
spearce <spearce@csumb.edu>
2019-08-14 20:16:08 -0400

    Meh

--- app.js
+++ app.js
@@ -538,15 +538,15 @@ app.post("/adminAuthenticate", async function(req,res) {
 // app.listen("8081", "0.0.0.0", function(){
 //    console.log("Running Express Server...")
 // });
-// var port = process.env.PORT || 8000;
-// server.listen(port, function() {
-//     console.log("App is running on port " + port);
-// });
-
-const port = process.env.PORT;
-server.listen(port, () => {
+var port = process.env.PORT;
+server.listen(port, function() {
     console.log("App is running on port " + port);
 });
+
+// const port = process.env.PORT;
+// server.listen(port, () => {
+//     console.log("App is running on port " + port);
+// });
 
 
 
